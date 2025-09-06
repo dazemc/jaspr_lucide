6a429eb09c07587ed6fbab5ceb4caac5cd0ebf2b
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-09-06 02:39:33 -0700

    user can't be in user when it's being run as a user........ and actually get the hash

--- tool/cron.sh
+++ tool/cron.sh
@@ -1,54 +1,62 @@
 #!/bin/env bash
 previousHash=$(cat '.lucidehash')
-currentHash=$(git -C '../src/lucide-icons/' rev-parse HEAD)
 startUpdate=true
 isUpdate=false
 
 function moveHere {
-    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    cd "$SCRIPT_DIR" || { echo "Could not move to $SCRIPT_DIR"; exit;}
+  SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+  cd "$SCRIPT_DIR" || {
+    echo "Could not move to $SCRIPT_DIR"
+    exit
+  }
 }
 
 function checkHashRecursive {
-    if [ "$previousHash" == "$currentHash" ]; then
-        if [ $startUpdate = true ]; then
-            updateSubmodule
-            checkHashRecursive
-        else
-            exit 1
-        fi
+  if [ "$previousHash" == "$(git -C '../src/lucide-icons/' rev-parse HEAD)" ]; then
+    echo "Saved hash: $previousHash"
+    echo "Current hash: $(git -C '../src/lucide-icons/' rev-parse HEAD)"
+    if [ $startUpdate = true ]; then
+      "Starting submodule update"
+      updateSubmodule
+      checkHashRecursive
     else
-        isUpdate=true
-        git -C '../src/lucide-icons/' rev-parse HEAD > './.lucidehash'
+      echo "No update found, exiting..."
+      exit 1
     fi
+  else
+    isUpdate=true
+    git -C '../src/lucide-icons/' rev-parse HEAD >'./.lucidehash'
+  fi
 }
 
 function updateSubmodule {
-    git -C '../' submodule update --remote
-    startUpdate=false
+  git -C '../' submodule update --remote
+  startUpdate=false
 }
 
 function build {
-    sh "./build.sh"
+  sh "./build.sh"
 }
 
 function commitPush {
-    git -C '../' add .
-    git -C '../' commit -m "Automated update"
-    git -C '../' push
+  git -C '../' add .
+  git -C '../' commit -m "Automated update"
+  git -C '../' push
 }
 
 function main {
-    moveHere
-    checkHashRecursive
-    if [ $isUpdate = true ]; then
-        build
-        commitPush
-        exit 0
-    else
-        echo "" >> /dev/null
-        # exit 1
-    fi
+  moveHere
+  checkHashRecursive
+  if [ $isUpdate = true ]; then
+    echo "starting build"
+    build
+    echo "commit and push"
+    # commitPush
+    exit 0
+  else
+    echo "" >>/dev/null
+    # exit 1
+  fi
 }
 
 main
