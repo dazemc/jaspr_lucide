bade1dc33382ad2a30b03cb4536ea097f5b88100
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-11-24 00:00:48 -0800

    Automated update: Add waves arrow up down (#3463)

---
+++ lib/generated_icons/waves_arrow_down.dart
@@ -0,0 +1,62 @@
+// GENERATED FILE DO NOT EDIT
+
+import 'dart:core' as core;
+import 'package:jaspr/jaspr.dart' as jaspr;
+
+class WavesArrowDown extends jaspr.StatelessComponent {
+  final jaspr.Unit? width;
+  final jaspr.Unit? height;
+  final core.String? viewBox;
+  final core.Map<core.String, core.String>? attributes;
+  final core.String? id;
+  final core.String? classes;
+  final jaspr.Styles? styles;
+  final core.Map<core.String, jaspr.EventCallback>? events;
+
+  WavesArrowDown({
+    this.width = const jaspr.Unit.pixels(24),
+    this.height = const jaspr.Unit.pixels(24),
+    this.viewBox = '0 0 24 24',
+    this.attributes,
+    this.id,
+    this.classes,
+    this.styles,
+    this.events,
+    super.key,
+  });
+
+  @core.override
+  jaspr.Component build(jaspr.BuildContext context) {
+    const defaultAttributes = {
+      'xmlns': 'http://www.w3.org/2000/svg',
+      'fill': 'none',
+      'stroke': 'currentColor',
+      'stroke-width': '2',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+    };
+    return jaspr.svg(
+      width: width,
+      height: height,
+      viewBox: viewBox,
+      key: key,
+      classes: classes,
+      styles: styles,
+      id: id,
+      events: events,
+      attributes: {...defaultAttributes, ...?attributes},
+      [
+        jaspr.path(d: 'M12 10L12 2', []),
+        jaspr.path(d: 'M16 6L12 10L8 6', []),
+        jaspr.path(
+          d: 'M2 15C2.6 15.5 3.2 16 4.5 16C7 16 7 14 9.5 14C12.1 14 11.9 16 14.5 16C17 16 17 14 19.5 14C20.8 14 21.4 14.5 22 15',
+          [],
+        ),
+        jaspr.path(
+          d: 'M2 21C2.6 21.5 3.2 22 4.5 22C7 22 7 20 9.5 20C12.1 20 11.9 22 14.5 22C17 22 17 20 19.5 20C20.8 20 21.4 20.5 22 21',
+          [],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/generated_icons/waves_arrow_up.dart
@@ -0,0 +1,62 @@
+// GENERATED FILE DO NOT EDIT
+
+import 'dart:core' as core;
+import 'package:jaspr/jaspr.dart' as jaspr;
+
+class WavesArrowUp extends jaspr.StatelessComponent {
+  final jaspr.Unit? width;
+  final jaspr.Unit? height;
+  final core.String? viewBox;
+  final core.Map<core.String, core.String>? attributes;
+  final core.String? id;
+  final core.String? classes;
+  final jaspr.Styles? styles;
+  final core.Map<core.String, jaspr.EventCallback>? events;
+
+  WavesArrowUp({
+    this.width = const jaspr.Unit.pixels(24),
+    this.height = const jaspr.Unit.pixels(24),
+    this.viewBox = '0 0 24 24',
+    this.attributes,
+    this.id,
+    this.classes,
+    this.styles,
+    this.events,
+    super.key,
+  });
+
+  @core.override
+  jaspr.Component build(jaspr.BuildContext context) {
+    const defaultAttributes = {
+      'xmlns': 'http://www.w3.org/2000/svg',
+      'fill': 'none',
+      'stroke': 'currentColor',
+      'stroke-width': '2',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+    };
+    return jaspr.svg(
+      width: width,
+      height: height,
+      viewBox: viewBox,
+      key: key,
+      classes: classes,
+      styles: styles,
+      id: id,
+      events: events,
+      attributes: {...defaultAttributes, ...?attributes},
+      [
+        jaspr.path(d: 'M12 2v8', []),
+        jaspr.path(
+          d: 'M2 15c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1',
+          [],
+        ),
+        jaspr.path(
+          d: 'M2 21c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1',
+          [],
+        ),
+        jaspr.path(d: 'm8 6 4-4 4 4', []),
+      ],
+    );
+  }
+}
