f005c906d021aafbe74fe1c9e330026259bce672
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-12-06 00:00:50 -0800

    Automated update: fix: fixed linting issues introduced in c4e5730bc40193035b45654c87433898846977ad (#3858)

--- lib/generated_icons/van.dart
+++ lib/generated_icons/van.dart
@@ -3,7 +3,7 @@
 import 'dart:core' as core;
 import 'package:jaspr/jaspr.dart' as jaspr;
 
-class Bubbles extends jaspr.StatelessComponent {
+class Van extends jaspr.StatelessComponent {
   final jaspr.Unit? width;
   final jaspr.Unit? height;
   final core.String? viewBox;
@@ -13,7 +13,7 @@ class Bubbles extends jaspr.StatelessComponent {
   final jaspr.Styles? styles;
   final core.Map<core.String, jaspr.EventCallback>? events;
 
-  Bubbles({
+  Van({
     this.width = const jaspr.Unit.pixels(24),
     this.height = const jaspr.Unit.pixels(24),
     this.viewBox = '0 0 24 24',
@@ -46,10 +46,17 @@ class Bubbles extends jaspr.StatelessComponent {
       events: events,
       attributes: {...defaultAttributes, ...?attributes},
       [
-        jaspr.path(d: 'M7.001 15.085A1.5 1.5 0 0 1 9 16.5', []),
-        jaspr.circle(cx: '18.5', cy: '8.5', r: '3.5', []),
-        jaspr.circle(cx: '7.5', cy: '16.5', r: '5.5', []),
-        jaspr.circle(cx: '7.5', cy: '4.5', r: '2.5', []),
+        jaspr.path(
+          d: 'M13 6v5a1 1 0 0 0 1 1h6.102a1 1 0 0 1 .712.298l.898.91a1 1 0 0 1 .288.702V17a1 1 0 0 1-1 1h-3',
+          [],
+        ),
+        jaspr.path(
+          d: 'M5 18H3a1 1 0 0 1-1-1V8a2 2 0 0 1 2-2h12c1.1 0 2.1.8 2.4 1.8l1.176 4.2',
+          [],
+        ),
+        jaspr.path(d: 'M9 18h5', []),
+        jaspr.circle(cx: '16', cy: '18', r: '2', []),
+        jaspr.circle(cx: '7', cy: '18', r: '2', []),
       ],
     );
   }
--- lib/generated_icons/weight_tilde.dart
+++ lib/generated_icons/weight_tilde.dart
@@ -3,7 +3,7 @@
 import 'dart:core' as core;
 import 'package:jaspr/jaspr.dart' as jaspr;
 
-class Bubbles extends jaspr.StatelessComponent {
+class WeightTilde extends jaspr.StatelessComponent {
   final jaspr.Unit? width;
   final jaspr.Unit? height;
   final core.String? viewBox;
@@ -13,7 +13,7 @@ class Bubbles extends jaspr.StatelessComponent {
   final jaspr.Styles? styles;
   final core.Map<core.String, jaspr.EventCallback>? events;
 
-  Bubbles({
+  WeightTilde({
     this.width = const jaspr.Unit.pixels(24),
     this.height = const jaspr.Unit.pixels(24),
     this.viewBox = '0 0 24 24',
@@ -46,10 +46,12 @@ class Bubbles extends jaspr.StatelessComponent {
       events: events,
       attributes: {...defaultAttributes, ...?attributes},
       [
-        jaspr.path(d: 'M7.001 15.085A1.5 1.5 0 0 1 9 16.5', []),
-        jaspr.circle(cx: '18.5', cy: '8.5', r: '3.5', []),
-        jaspr.circle(cx: '7.5', cy: '16.5', r: '5.5', []),
-        jaspr.circle(cx: '7.5', cy: '4.5', r: '2.5', []),
+        jaspr.path(
+          d: 'M6.5 8a2 2 0 0 0-1.906 1.46L2.1 18.5A2 2 0 0 0 4 21h16a2 2 0 0 0 1.925-2.54L19.4 9.5A2 2 0 0 0 17.48 8z',
+          [],
+        ),
+        jaspr.path(d: 'M7.999 15a2.5 2.5 0 0 1 4 0 2.5 2.5 0 0 0 4 0', []),
+        jaspr.circle(cx: '12', cy: '5', r: '3', []),
       ],
     );
   }
