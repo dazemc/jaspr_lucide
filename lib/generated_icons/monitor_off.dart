80eb392421da3c48231ae4464c4884d5263ecfa5
Daazed McFarland <daazedjmcfarland@gmail.com>
2026-01-11 00:01:02 -0800

    Automated update: chore(deps-dev): bump preact from 10.27.2 to 10.27.3 (#3955)

--- lib/generated_icons/monitor_off.dart
+++ lib/generated_icons/monitor_off.dart
@@ -47,11 +47,11 @@ class MonitorOff extends jaspr.StatelessComponent {
       events: events,
       attributes: {...defaultAttributes, ...?attributes},
       [
-        dom.path(d: 'M17 17H4a2 2 0 0 1-2-2V5c0-1.5 1-2 1-2', []),
-        dom.path(d: 'M22 15V5a2 2 0 0 0-2-2H9', []),
-        dom.path(d: 'M8 21h8', []),
         dom.path(d: 'M12 17v4', []),
+        dom.path(d: 'M17 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 1.184-1.826', []),
         dom.path(d: 'm2 2 20 20', []),
+        dom.path(d: 'M8 21h8', []),
+        dom.path(d: 'M8.656 3H20a2 2 0 0 1 2 2v10a2 2 0 0 1-.293 1.042', []),
       ],
     );
   }
