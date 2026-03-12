80dbd5782a07af9e0d3c6dd00dfbe3fbea743056
Daazed McFarland <daazedjmcfarland@gmail.com>
2026-03-12 00:00:58 -0700

    Automated update: fix(icons): changed `school` icon (#4124)

--- lib/generated_icons/school.dart
+++ lib/generated_icons/school.dart
@@ -48,13 +48,13 @@ class School extends jaspr.StatelessComponent {
       attributes: {...defaultAttributes, ...?attributes},
       [
         dom.path(d: 'M14 21v-3a2 2 0 0 0-4 0v3', []),
-        dom.path(d: 'M18 5v16', []),
+        dom.path(d: 'M18 4.933V21', []),
         dom.path(d: 'm4 6 7.106-3.79a2 2 0 0 1 1.788 0L20 6', []),
         dom.path(
           d: 'm6 11-3.52 2.147a1 1 0 0 0-.48.854V19a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-5a1 1 0 0 0-.48-.853L18 11',
           [],
         ),
-        dom.path(d: 'M6 5v16', []),
+        dom.path(d: 'M6 4.933V21', []),
         dom.circle(cx: '12', cy: '9', r: '2', []),
       ],
     );
